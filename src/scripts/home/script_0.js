ae89dbf980135722b5d229b871d493104021892e
chao <chao9420@gmail.com>
2015-07-03 23:07:29 +0800

    fix delete

--- src/scripts/home/script_0.js
+++
@@ -1,3 +0,0 @@
-$(window).ready(function() {
-	$("body").append('Script_0.js is being executed.<br/>');
-});
--- src/scripts/home/script_2.js
+++
@@ -1,4 +0,0 @@
-$(window).ready(function() {
-	$("body").append('Script_2.js is being executed.<br/>');
-});
-
